3b2e07163bca22d24680f0fbc9f9c1e6bcb7b300
Sage <indiedragoness@protonmail.com>
2021-05-16 14:17:31 -0500

    Create target_generator.py

---
+++ envs/target_generator.py
@@ -0,0 +1,18 @@
+from gym import Env
+from gym.spaces import Discrete, Box
+import numpy as np
+import random
+
+class TargetGenerator(Env):
+    def __init__(self):
+        pass
+    def step(self):
+        pass
+    def render(self):
+        pass
+    def reset(self):
+        pass
+
+
+
+
